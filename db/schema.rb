28be64961b76f9d0e16814e74fcf29e0d7d29cf7
Gabriel <gabriel.belgamo@codeminer.com.br>
2020-02-28 16:26:38 -0300

    add secure password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_28_144152) do
+ActiveRecord::Schema.define(version: 2020_02_28_192127) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_02_28_144152) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "comments", "users"
