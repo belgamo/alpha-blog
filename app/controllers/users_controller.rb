921f66e0ea61814dd2c2959a4f14c9938b3d3606
Gabriel <gabriel.belgamo@codeminer.com.br>
2020-03-03 22:56:56 -0300

    admin mode

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -37,6 +38,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+
+    if current_user == @user 
+      flash[:danger] = "You can't delete yourself"
+      redirect_to users_path and return
+    end
+
+    @user.destroy    
+    flash[:danger] = "User and all articles created by user have been deleted"
+    redirect_to users_path
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
@@ -49,9 +63,16 @@ class UsersController < ApplicationController
     end
 
     def require_same_user
-      if current_user != @user
+      if current_user != @user and !current_user.admin?
         flash[:danger] = "You can only edit your own account"
         redirect_to root_path
+      end
+    end
+
+    def require_admin
+      if !current_user.admin?
+        flash[:danger] = "Only admin users can perform that action"
+        redirect_to users_path
       end
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_28_192127) do
+ActiveRecord::Schema.define(version: 2020_03_03_234223) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -22,7 +25,7 @@ ActiveRecord::Schema.define(version: 2020_02_28_192127) do
 
   create_table "comments", force: :cascade do |t|
     t.text "description"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_comments_on_user_id"
@@ -34,6 +37,7 @@ ActiveRecord::Schema.define(version: 2020_02_28_192127) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "comments", "users"
